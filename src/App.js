9a0989181b19e3ed5aa9b6e5c403809f71411495
Cam Chambers <cam_chambers@outlook.com>
2021-07-24 07:12:36 -0400

    Added nanoid for creating unique ids

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { nanoid } from "nanoid";
 import React, { useState } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
@@ -6,7 +7,7 @@ import Todo from "./components/Todo";
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
   function addTask(name){
-    const newTask = { id: "id", name: name, completed: false };
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask]);
   }
   const taskList = tasks.map(task => (
