090a4629a97a581ef355cced9785c85a55167c70
Cam Chambers <cam_chambers@outlook.com>
2021-07-24 11:53:24 -0400

    Added logic to distinguish between one task and multiple tasks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import { nanoid } from "nanoid";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 const FILTER_MAP = {
   All: () => true,
@@ -67,8 +75,15 @@ function App(props) {
       setFilter={setFilter}
     />
   ));
-  const taskNoun = taskList.length !== 1 ? 'tasks' : 'task';
-  const headingText = `${taskList.length} tasks remaining`;
+  const taskNoun = taskList.length !== 1 ? 'Tasks' : 'Task';
+  const headingText = `${taskList.length} ${taskNoun}`;
+  const listHeadingRef = useRef(null);
+  const prevTaskLength = usePrevious(tasks.length);
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
   return (
     <div className="todoapp stack-large">
       <h1>To Do List</h1>
@@ -76,7 +91,9 @@ function App(props) {
       <div className="filters btn-group stack-exception">
       {filterList}
       </div>
-      <h2 id="list-heading">{headingText}</h2>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
+      {headingText}
+      </h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
