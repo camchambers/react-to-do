714cbd47c68a5856e40da3592486e7ada55f6721
Cam Chambers <cam_chambers@outlook.com>
2021-07-24 11:52:16 -0400

    Added accessability features

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 export default function Todo(props) {
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState('');
+  const editFieldRef = useRef(null);
+  const editButtonRef = useRef(null);
+  const wasEditing = usePrevious(isEditing);
   function handleChange(e) {
     setNewName(e.target.value);
   }
@@ -24,16 +35,17 @@ export default function Todo(props) {
           type="text"
           value={newName}
           onChange={handleChange}
+          ref={editFieldRef}
         />
       </div>
       <div className="btn-group">
-        <button
-          type="button"
-          className="btn todo-cancel"
-          onClick={() => setEditing(false)}
-          >
-          Cancel
-          <span className="visually-hidden">renaming {props.name}</span>
+      <button
+        type="button"
+        className="btn"
+        onClick={() => setEditing(true)}
+        ref={editButtonRef}
+        >
+        Edit <span className="visually-hidden">{props.name}</span>
       </button>
         <button type="submit" className="btn btn__primary todo-edit">
           Save
@@ -69,5 +81,13 @@ export default function Todo(props) {
         </div>
     </div>
   );
+  useEffect(() => {
+    if (!wasEditing && isEditing) {
+      editFieldRef.current.focus();
+    }
+    if (wasEditing && !isEditing) {
+      editButtonRef.current.focus();
+    }
+  }, [wasEditing, isEditing]);  
   return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
   }
